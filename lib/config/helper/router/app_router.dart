872c4d75934631a4697ccb5411b4193dd0175bfe
Manuel Walter Navarro Z <manuel08n@gmail.com>
2023-08-01 11:42:54 -0500

    advance project router screen

--- lib/config/helper/router/app_router.dart
+++ lib/config/helper/router/app_router.dart
@@ -1,19 +1,35 @@
-
 import 'package:go_router/go_router.dart';
 import '../../../presentation/screens.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/',
-  routes: [
-    GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    name: HomeScreen.name,
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
       path: '/theme',
       name: ThemeScreen.name,
-      builder: ((context, state) => const ThemeScreen())
-    )
-  ]
-);
+      builder: ((context, state) => const ThemeScreen())),
+  GoRoute(
+    path: '/monthemployment',
+    name: MonthScreen.name,
+    builder: (context, state) => const MonthScreen(),
+  ),
+  GoRoute(
+      path: '/cv',
+      name: CvScreen.name,
+      builder: (context, state) => const CvScreen()),
+  GoRoute(
+      path: '/suscription',
+      name: SuscriptionScreen.name,
+      builder: (context, state) => const SuscriptionScreen()),
+  GoRoute(
+      path: '/message',
+      name: MessageScreen.name,
+      builder: (context, state) => const MessageScreen()),
+  GoRoute(
+      path: '/world',
+      name: WorldScreen.name,
+      builder: (context, state) => const WorldScreen())
+]);
--- lib/presentation/screens.dart
+++ lib/presentation/screens.dart
@@ -1,3 +1,7 @@
+export 'package:router_basic_app/presentation/screen/cv/cv_screen.dart';
+export 'package:router_basic_app/presentation/screen/message/message_screen.dart';
+export 'package:router_basic_app/presentation/screen/month_employment/month_employment_screen.dart';
+export 'package:router_basic_app/presentation/screen/suscription/suscription_screen.dart';
+export 'package:router_basic_app/presentation/screen/world/world_screen.dart';
 export 'package:router_basic_app/presentation/screen/theme/theme_screen.dart';
-
 export 'package:router_basic_app/presentation/screen/home/home_screen.dart';
